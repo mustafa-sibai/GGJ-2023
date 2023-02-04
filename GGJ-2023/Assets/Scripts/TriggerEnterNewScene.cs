988a8d931a5a10d6db1aab40815827e7f0914fb1
Anna Tookey <40760387+OokeyTookey@users.noreply.github.com>
2023-02-04 22:03:35 +0400

    - Scene transitions - level design

---
+++ GGJ-2023/Assets/Scripts/TriggerEnterNewScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnterNewScene : MonoBehaviour
+{
+
+    SceneLoading sceneLoadScript;
+
+    void Start()
+    {
+        sceneLoadScript = FindObjectOfType<SceneLoading>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+        sceneLoadScript.LoadTheScene();
+
+        }
+    }
+}
